f2bb97ed743a934024f7319628cafb0eb40df3e9
Pavel-nineseven <pavel@nineseven.ru>
2017-11-16 19:31:34 +0300

    last

--- app/js/interface.js
+++ app/js/interface.js
@@ -226,7 +226,45 @@ $(document).ready(function() {
 
 
 
-
+	//SQUARES HOVER
+	if ($('.squares').length>0) {
+	    var bArray = [];
+	    // Define a size array, this will be used to vary bubble sizes
+	    var sArray = [4,6,8,10];
+	 
+	    // Push the header width values to bArray
+	    for (var i = 0; i < $('.squares').width(); i++) {
+	        bArray.push(i);
+	    }
+	     
+	    // Function to select random array element
+	    // Used within the setInterval a few times
+	    function randomValue(arr) {
+	        return arr[Math.floor(Math.random() * arr.length)];
+	    }
+	 
+	    // setInterval function used to create new bubble every 350 milliseconds
+	    setInterval(function(){
+	         
+	        // Get a random size, defined as variable so it can be used for both width and height
+	        var size = randomValue(sArray);
+	        // New bubble appeneded to div with it's size and left position being set inline
+	        // Left value is set through getting a random value from bArray
+	        $('.squares').append('<div class="i-square" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
+	         
+	        // Animate each bubble to the top (bottom 100%) and reduce opacity as it moves
+	        // Callback function used to remove finsihed animations from the page
+	        $('.i-square').animate({
+	            'bottom': '100%',
+	            'opacity' : '-=0.7'
+	        }, 3000, function(){
+	            $(this).remove()
+	        }
+	        );
+	 
+	 
+	    }, 350);
+    }
 
 
 
